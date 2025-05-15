7df91ba221274e50868083ab28e98c1073478ced
Jason Pavlopoulos <Jasonpavlop1@gmail.com>
2025-05-15 11:55:33 +0300

    Vercel Analytics
    - Added Vercel Analytics

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -3,6 +3,7 @@ import AboutSection from "@/components/sections/about-section";
 import SkillSection from "@/components/sections/skills-section";
 import ExperienceSection from "@/components/sections/experience-section";
 import ProjectsSection from "@/components/sections/projects-section";
+import { Analytics } from "@vercel/analytics/next";
 
 export const dynamic = "force-dynamic";
 
@@ -10,6 +11,7 @@ const LandingPage = (): ReactElement => (
   <main className="min-h-screen flex">
     {/* Content - adjusted padding for sidebar */}
     <div className="relative w-full px-7 md:px-10 xl:px-16 pt-32 flex flex-col transition-all transform-gpu overflow-hidden">
+      <Analytics />
       <AboutSection />
       <SkillSection />
       <ExperienceSection />
